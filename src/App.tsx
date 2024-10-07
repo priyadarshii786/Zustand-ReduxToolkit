618b3a1fa45f547510df5bb6c9010118346f1ca9
priyadarshii786 <tathagatp918@gmail.com>
2024-10-08 02:39:02 +0530

    C-02; Project Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import { Container, Typography, Box } from "@mui/material";
+import AddHabitForm from "./components/add-habit-form";
+import HabitList from "./components/habit-list";
+import HabitStats from "./components/habit-stats";
+import useHabitStore from "./store/store";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const { fetchHabits } = useHabitStore();
+
+  useEffect(() => {
+    fetchHabits();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Container maxWidth="md">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h2" component="h1" gutterBottom align="center">
+          Habit Tracker
+        </Typography>
+        <AddHabitForm />
+        <HabitList />
+        <HabitStats />
+      </Box>
+    </Container>
+  );
+};
 
-export default App
+export default App;
